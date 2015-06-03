68937b1e450b382165a548145d8d05202699d130
Fred Thompson <fred.thompson@buildempire.co.uk>
2015-06-03 11:37:45 +0100

    Finish InitialBuild

---
+++ metadata.rb
@@ -0,0 +1,17 @@
+name             'cookbook_npm'
+maintainer       'Fred Thompson'
+maintainer_email 'fred.thompson@buildempire.co.uk'
+license          'Apache 2.0'
+description      'The NPM cookbook, adds everything you need to use NPM with vagrant box, including symlinks and package provision.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
+recipe 'cookbook_npm', 'The Ember NPM, adds everything you need to use NPM with vagrant box, including symlinks and package provision.'
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
+
+%w{nodejs}.each do |cb|
+  depends cb
+end
---
+++ recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: cookbook_npm
+# Recipe:: default
+#
+
+include_recipe 'cookbook_npm::symlinks'
+include_recipe 'cookbook_npm::npm'
