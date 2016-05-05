fa42e9e54f664ec17f95116af611e0d5c0e93aba
Fred Thompson <fred.thompson@buildempire.co.uk>
2016-05-05 20:31:54 +0100

    Upped the default value of node to 4.4.3.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,8 +4,8 @@ default['cookbook_npm']['symlinks'] = []
 
 # Set the default node version and matching  SHA-256checksum
 default['cookbook_npm']['nodejs']['install_method'] = 'source'
-default['cookbook_npm']['nodejs']['version'] = '4.1.0'
-default['cookbook_npm']['nodejs']['checksum'] = '453005f64ee529f7dcf1237eb27ee2fa2415c49f5c9e7463e8b71fba61c5b408'
+default['cookbook_npm']['nodejs']['version'] = '4.4.3'
+default['cookbook_npm']['nodejs']['checksum'] = '8e67b95721aab7bd721179da2fe5dd97f9acc1306c15c9712ee103bcd6381638'
 
 # Create an array of npm packages to install
 default['cookbook_npm']['npm_packages'] = []
