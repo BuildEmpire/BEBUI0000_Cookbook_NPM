086767d3693d07ac596d0010f49cd5c14545ef83
Fred Thompson <fred.thompson@buildempire.co.uk>
2016-06-21 11:05:32 +0100

    Attempting to fix the reference.

--- recipes/npm.rb
+++ recipes/npm.rb
@@ -10,7 +10,7 @@ include_recipe "nodejs"
 # If you want to add the packages, please use npm_packages as part of the nodejs chef cookbook
 # (https://github.com/redguide/nodejs)
 Array(node['cookbook_npm']['package_symlinks']).each_with_index do |package_name, index|
-  link File.join(default['ark']['prefix_root'], 'bin', package_name) do
-    to File.join(default['ark']['prefix_root'], 'nodejs-binary', 'bin', package_name)
+  link File.join(node['ark']['prefix_root'], 'bin', package_name) do
+    to File.join(node['ark']['prefix_root'], 'nodejs-binary', 'bin', package_name)
   end
 end
